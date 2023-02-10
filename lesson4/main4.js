e881f26047eda215948d892e15d00328ae34cea7
BohdanaKh <dananvm@gmail.com>
2023-02-11 00:19:13 +0200

    add homework4

--- lesson4/main4.js
+++ lesson4/main4.js
@@ -50,11 +50,64 @@ function arrayPrimitives(array) {
     document.write(` </ul>`);
 }
 arrayPrimitives(['dffh', 445, -18, 'dsfd', false]);
-// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок
+
+function arrayOfUsers(array){
+    for (let item of array) {
+        document.write(`<div> id:${ item.id}, name: ${item.name}, age: ${item.age} </div>`);
+    }
+}
+
+let participants=[
+    {id:1, name: 'iren', age: 23},
+    {id:2, name: 'vasya', age: 30},
+    {id:3, name: 'kolya', age: 29},
+    {id:4, name: 'olya', age: 28},
+    {id:5, name: 'max', age: 30}
+];
+arrayOfUsers(participants);
 
 // - створити функцію яка повертає найменьше число з масиву
+let nums=[-88,-470,-52,2,6,458,0];
+function minOfNumbers(array){
+    let min = array[0];
+    for (let i=1; i<array.length; i++) {
+        if (array[i]<min){
+            min=array[i];
+        }
+    }
+    return min;
+}
+console.log(minOfNumbers(nums));
 // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
-// - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
+function sum(arr){
+    let summ=arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        summ+=arr[i];
+    }
+    return summ;
+}
+console.log(sum([10,20,30,40,50]));
+// - створити функцію swap(arr,index1,index2). Функція міняє місцями значення у відповідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+function swap(arr,index1,index2) {
+    [arr[index1],arr[index2]]= [arr[index2],arr[index1]];
+    console.log(arr);
+    }
+swap([11,22,33,44,55],2,4);
+
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
+function exchange(sumUAH,currencyValues,exchangeCurrency) {
+    for (let i = 0; i < currencyValues.length; i++) {
+        const currencyvalue = currencyValues[i];
+        if (currencyvalue.currency===exchangeCurrency){
+            let sum = sumUAH/currencyvalue.value;
+            console.log(sum);
+        }
+        else{console.log('????');
+        }
+    }
+}
+
+exchange(20000,[{currency:'USD',value:8},{currency:'EUR',value:10}],'EUR');
